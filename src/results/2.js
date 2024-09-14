6c3f1a828883a9f6b074c9cfd50b68138333c4fc
Efonovia <lordnovia@gmail.com>
2024-09-14 09:59:31 +0100

    gemini test results

---
+++ src/results/2.js
@@ -0,0 +1,83 @@
+[
+    {
+        "id": "d8f24427-7134-42d8-a665-141138793db5",
+        "type": "dropdown",
+        "text": "How did you hear about our zoo?",
+        "required": true,
+        "options": [
+            "Social media",
+            "Friends/Family",
+            "Website",
+            "Advertisement",
+            "Other"
+        ]
+    },
+    {
+        "id": "fb0df26e-e853-4ae7-8bd8-b37a06dda10f",
+        "type": "rating",
+        "text": "How would you rate your overall experience today?",
+        "required": true,
+        "maxRating": 5
+    },
+    {
+        "id": "f5108cd5-f3cd-4880-809c-2d779734ae6f",
+        "type": "slider",
+        "text": "How likely are you to recommend this zoo to others?",
+        "required": true,
+        "min": 0,
+        "max": 10,
+        "step": 1
+    },
+    {
+        "id": "2656f3a0-3322-44fd-8ff0-48af1a8d0501",
+        "type": "checkboxes",
+        "text": "Which animal exhibit did you enjoy the most?",
+        "required": true,
+        "options": [
+            "Lions",
+            "Elephants",
+            "Birds",
+            "Reptiles",
+            "Aquatic animals",
+            "Other"
+        ]
+    },
+    {
+        "id": "99da50b3-786c-4434-8afb-2df63a7f712c",
+        "type": "yes_no",
+        "text": "Were the animal enclosures clean and well-maintained?",
+        "required": true
+    },
+    {
+        "id": "5b7318a0-d173-4ef8-a457-bab3fcf22b12",
+        "type": "mcq",
+        "text": "Which of the following animals would you like to see more of?",
+        "required": true,
+        "options": [
+            "Big cats",
+            "Primates",
+            "Reptiles",
+            "Birds",
+            "Marine life"
+        ]
+    },
+    {
+        "id": "e92512af-6e2f-43d5-937f-7c06acbec32b",
+        "type": "rating",
+        "text": "How would you rate the cleanliness of the zoo facilities (bathrooms, restaurants, etc.)?",
+        "required": true,
+        "maxRating": 5
+    },
+    {
+        "id": "e0ec1c90-880f-4a2f-8de5-79a77dd0dd6f",
+        "type": "yes_no",
+        "text": "Were the signs and maps around the zoo helpful?",
+        "required": true
+    },
+    {
+        "id": "de3be21e-fa2a-4185-bc0d-05d828e4e00c",
+        "type": "long_answer",
+        "text": "Do you have any suggestions for improving the zoo experience?",
+        "required": false
+    }
+]
